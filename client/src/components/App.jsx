a6db82d1921dcb77295497b7328e0c92eac66157
Gareth Rogers <garethdavisrogers@gmail.com>
2021-06-11 12:59:26 -0500

    sends text upon activation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      active: false,
+    };
+    this.handleActivateFRS = this.handleActivateFRS.bind(this);
+  }
+
+  handleActivateFRS() {
+    debugger;
+    this.setState({ active: true });
+    axios.get("/activate").then((response) => console.log(response));
   }
 
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <button onClick={this.handleActivateFRS}>Activate</button>
+      </div>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
 app.use(express.static("./client/public"));
 
-app.get('/', (req, res)=>{
-  res.send('hey')
-})
+app.get("/", (req, res) => {
+  res.send("hey");
+});
+
+app.get("/activate", (req, res) => {
+  client.messages
+    .create({
+      body: "Emergency services have been notified",
+      from: "+12562693589",
+      to: "+14785503644",
+    })
+    .then((message) => console.log(message.sid));
+});
+
 app.listen(port, () => {
   console.log("node is listening");
 });
